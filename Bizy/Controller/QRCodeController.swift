9c5a18add64ea8b220ce9438cf12dbb4f626f68c
Wolf <obedappiah-agyeman@Obeds-MBP.wv.cc.cmu.edu>
2018-11-09 02:44:38 -0500

    Sending to Mai

--- Bizy/Controller/QRCodeController.swift
+++ Bizy/Controller/QRCodeController.swift
@@ -5,5 +5,19 @@
 //  Created by Wolf on 11/9/18.
 //  Copyright © 2018 Wolf. All rights reserved.
 //
+import UIKit
 
-import Foundation
+class QRCodeController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+}
